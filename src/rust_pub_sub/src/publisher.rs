64b2c674c6f1ad9f2bc6494bfa570ce3566a43f5
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-01-06 23:55:48 -0800

    fixed struct implementation

--- src/rust_pub_sub/src/publisher.rs
+++ src/rust_pub_sub/src/publisher.rs
@@ -1,33 +1,35 @@
 use std::env;
 use opencv::{prelude::*, videoio};
 use anyhow::{Error, Result};
-use cv_bridge::CvImage;
+use cv_bridge::
 
+struct CameraPublisher {
+    node: rclrs::Node,
+    _publisher: rclrs::Publisher<Image>,
+}
+
+impl CameraPublisher {
+    fn new(context: &rclrs::Context) -> Result<Self, Error> {
+        let mut node = rclrs::Node::new(context, "camera_subscriber")?;
+        let _publisher = node.create_publisher::<sensor_msgs::msg::Image>("topic", rclrs::QOS_PROFILE_DEFAULT)?;
+        Ok(Self{node, _subscription, gui})
+    };
+};
+ 
 fn main() -> Result<(), Error> {
-    let context = rclrs::Context::new(env::args())?;
-
-    let node = rclrs::create_node(&context, "rust_publisher")?;
-
-    let publisher =
-        node.create_publisher::<sensor_msgs::msg::Image>("topic", rclrs::QOS_PROFILE_DEFAULT)?;
-
-    let mut publish_count: u32 = 0;
-
-
+    let context = rclrs::Context::new(env::args())?;        
+    let camera_subscriber = CameraPublisher::new(context)?;
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
-    
     if !videoio::VideoCapture::is_opened(&cam) {
-		Error
-	} 
+        Err(())
+    }
     while context.ok() {
-        publish_count += 1;
-        println!("Publishing frame {}!", &publish_count);
-		    let mut frame = Mat::default();
-		    cam.read(&mut frame)?;
+        println!("Publishing frame!");
+        let mut frame = Mat::default();
+        cam.read(&mut frame)?;
         let msg = CvImage::from_cvmat(frame).into_imgmsg();
         publisher.publish(msg)?;
         std::thread::sleep(std::time::Duration::from_millis(500));
-	  }
-
+	}
     Ok(())
 }
--- src/rust_pub_sub/src/subscriber.rs
+++ src/rust_pub_sub/src/subscriber.rs
@@ -1,35 +1,43 @@
 use std::env;
+use sensor_msgs::msg::Image
 use opencv::{highgui, prelude::*};
 use cv_bridge::CvImage;
 use anyhow::{Error, Result};
 
+struct CameraSubscriber {
+    node: rclrs::Node,
+    _subscription: Arc<rclrs::Subscription<Image>>,
+    gui: bool
+}
+
+impl CameraSubscriber {
+    fn new(context: &rclrs::Context) -> Result<Self, Error> {
+        let mut node = rclrs::Node::new(context, "camera_subscriber")?;
+        let _subscription = { // Create a new shared pointer instance that will be owned by the closure
+            node.create_subscription(
+                "topic",
+                rclrs::QOS_PROFILE_DEFAULT,
+                move |msg: Image| {
+                    printl!("Recieving new image!");
+                    if self.gui {
+                        let frame = CvImage::from_imgmsg(msg).as_cvmat("bgr8".to_string());
+                        if frame.size().width > 0 {
+                            highgui::imshow(window, &frame);
+                        }
+                        let key = highgui::wait_key(10)?;
+                    }
+                }
+            )
+        },
+        let gui = false;
+        Ok(Self{node, _subscription, gui})
+    };
+};
+
 fn main() -> Result<(), Error> {
     let context = rclrs::Context::new(env::args())?;
-    let window = "video capture";
-	highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
-    let mut node = rclrs::create_node(&context, "rust_subscriber")?;
-
-    let mut num_messages: usize = 0;
-
-    let _subscription = node.create_subscription::<sensor_msgs::msg::Image, _>(
-        "topic",
-        rclrs::QOS_PROFILE_DEFAULT,
-        move |msg: sensor_msgs::msg::Image| {
-            num_messages += 1;
-            printl!("(Got {} messages so far)", &num_messages);
-            let gui = false;
-            if gui {
-                let frame = CvImage::from_imgmsg(msg).as_cvmat("bgr8".to_string());
-                if frame.size().width > 0 {
-                    highgui::imshow(window, &frame);
-                }
-                let key = highgui::wait_key(10);
-            }
-            Ok(())
-        },
-    )?;
-
-    rclrs::spin(&node).map_err(|err| err.into());
-
+    let camera_subscriber = CameraSubscriber::new(context);
+	highgui::named_window("video capture", highgui::WINDOW_AUTOSIZE)?;
+    rclrs::spin(&camera_subscriber.node)?;
     Ok(())
 }
