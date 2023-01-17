d5c0c56dd03bdc9259e6abff257d5021d87e333d
Austin Lake <53884490+austinlake04@users.noreply.github.com>
2023-01-17 01:06:01 -0800

    changed library usage in server node binaries

--- science_servers_rs/src/microscope.rs
+++ science_servers_rs/src/microscope.rs
@@ -1,6 +1,6 @@
-use science_servers_rs::CameraClient;
+use science_servers_rs::CameraServer;
 
 fn main() {
-    let server = CameraClient::new(subsystem="science", device="microscope", camera_settings=CameraClient::define_settings(frame_height=640, frame_height=480, fps=30));
+    let server = CameraServer::new(subsystem="science", device="microscope", camera_settings=CameraServer::define_settings(frame_height=640, frame_height=480, fps=30));
     server.run();
 }
--- science_servers_rs/src/stepper_motor_server.rs
+++ science_servers_rs/src/stepper_motor_server.rs
@@ -1,6 +1,6 @@
-use science_servers_rs::CameraClient;
+use science_servers_rs::PositionClient;
 
 fn main() {
-    let server = CameraClient::new(subsystem="science", device="stepper_motor", camera_settings=CameraClient::define_settings(frame_height=640, frame_height=480, fps=30));
+    let server = PositionClient::new(subsystem="science", device="stepper_motor");
     server.run();
 }
